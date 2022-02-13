99c3029557f0e78e0885ca78133cb307dd14d82d
Aarafat Islam <99079792+aarafat27@users.noreply.github.com>
2022-02-14 00:00:56 +0600

    Add files via upload

---
+++ LAB-3.1.m
@@ -0,0 +1,12 @@
+﻿pkg load communications;
+fs = 10e3;
+t = 0:1/fs:0.1;
+f = 10; 
+sig = 2*sin(2*pi*f*t);
+partition = [-1.5, -0.5, 0.5, 1.5]; 
+
+codebook = [-2:2];
+[i,xq] = quantiz(sig,partition,codebook); 
+
+plot(t,sig,'x',t,xq,'.')
+legend('Original signal','Quantized signal');
